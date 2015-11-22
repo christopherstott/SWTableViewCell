f87faaa0a40d9bda672e71cd627c95cbdd66cdc9
Christopher Stott <christopherstott@gmail.com>
2015-11-21 17:12:23 -0800

    Add stub framework

---
+++ SWTableViewCellStubFramework/SWTableViewCellStubFramework.h
@@ -0,0 +1,19 @@
+//
+//  SWTableViewCellStubFramework.h
+//  SWTableViewCellStubFramework
+//
+//  Created by Chris on 2015-11-21.
+//  Copyright © 2015 Chris Wendel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWTableViewCellStubFramework.
+FOUNDATION_EXPORT double SWTableViewCellStubFrameworkVersionNumber;
+
+//! Project version string for SWTableViewCellStubFramework.
+FOUNDATION_EXPORT const unsigned char SWTableViewCellStubFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWTableViewCellStubFramework/PublicHeader.h>
+
+
